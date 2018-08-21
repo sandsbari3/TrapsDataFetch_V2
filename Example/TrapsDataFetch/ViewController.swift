a8a3ed4d53e35980ac4f9d841d654f3a8c738651
Sandesh Ramakrishna <SRamakrishna@wilburellis.com>
2018-08-21 11:56:00 -0700

    Initial commit

---
+++ Example/TrapsDataFetch/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TrapsDataFetch
+//
+//  Created by sandsbari3 on 08/21/2018.
+//  Copyright (c) 2018 sandsbari3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
